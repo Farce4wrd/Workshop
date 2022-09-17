c28bc491d3e184dd746bb5e476deb41a229b64f9
Farce4wrd <ipeterokolocha@gmail.com>
2022-09-16 23:53:17 -0400

    Created Spring Project and now added data and schema sql files

---
+++ GrizzlyWeb/src/main/java/com/grizzly/GrizzlyWeb/GrizzlyWebApplication.java
@@ -0,0 +1,13 @@
+package com.grizzly.GrizzlyWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrizzlyWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrizzlyWebApplication.class, args);
+	}
+
+}
---
+++ GrizzlyWeb/src/test/java/com/grizzly/GrizzlyWeb/GrizzlyWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grizzly.GrizzlyWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrizzlyWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
